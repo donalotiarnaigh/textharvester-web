31de019e49559cb6bd7ad7ffe9472dcbaa1895f3
dtcurragh <97108312+dtcurragh@users.noreply.github.com>
2023-12-26 21:05:14 +0000

    wide ranging changes to server.js working towards multifile processing
    Enhanced server.js with improved logging and error handling for file uploads and processing. Refactored processing code to handle multiple files concurrently. Improved error handling for OpenAI API responses and file operations. Updated progress tracking and status reporting. Updated documentation for clearer understanding.

--- server.js
+++ server.js
@@ -21,33 +21,51 @@ const storage = multer.diskStorage({
   },
 });
 
-const upload = multer({ storage: storage });
+const upload = multer({ storage: storage }).array("file", 10); // Set a limit for the number of files (e.g., 10)
 
 app.use(express.static("public"));
 
-app.post("/upload", upload.single("file"), (req, res) => {
-  if (req.file) {
-    console.log(`Received file: ${req.file.originalname}`);
-    clearResultsFile();
-    res.redirect("/processing.html");
-    processFile(req.file.path);
-  } else {
-    console.log("No file uploaded.");
-    res.status(400).send("No file uploaded.");
-  }
+app.post("/upload", (req, res) => {
+  upload(req, res, async (err) => {
+    if (err) {
+      console.error("Error uploading files:", err);
+      return res.status(500).send("Error uploading files.");
+    }
+
+    if (req.files && req.files.length > 0) {
+      const totalFiles = req.files.length;
+      console.log(`Received ${totalFiles} files.`);
+      clearResultsFile(); // Clear results.json before processing new files
+
+      try {
+        for (const file of req.files) {
+          console.log(`Processing file: ${file.originalname}`);
+          await processFile(file.path, totalFiles);
+        }
+
+        res.redirect("/processing.html");
+      } catch (error) {
+        console.error("Error processing files:", error);
+        res.status(500).send("Error processing files.");
+      }
+    } else {
+      console.log("No files uploaded.");
+      res.status(400).send("No files uploaded.");
+    }
+  });
 });
 
 function clearResultsFile() {
   const resultsPath = "./data/results.json";
   try {
-    fs.writeFileSync(resultsPath, JSON.stringify({}));
+    fs.writeFileSync(resultsPath, JSON.stringify([]));
     console.log("Cleared results.json file.");
   } catch (err) {
     console.error("Error clearing results.json file:", err);
   }
 }
 
-async function processFile(filePath) {
+async function processFile(filePath, totalFiles) {
   const base64Image = fs.readFileSync(filePath, { encoding: "base64" });
   try {
     console.log(`Processing file: ${filePath}`);
@@ -59,7 +77,7 @@ async function processFile(filePath) {
           content: [
             {
               type: "text",
-              text: "ou're an expert in OCR and are working in a heritage/genealogy context assisting in data processing post graveyard survey.Examine these images and extract the handwritten text from the inscription field for each memorial number-no other fields..Respond in JSON format only.e.g {memorial_number: 69, inscription: SACRED HEART OF JESUS HAVE MERCY ON THE SOUL OF THOMAS RUANE LISNAGROOBE WHO DIED APRIL 16th 1923 AGED 74 YRS AND OF HIS WIFE MARGARET RUANE DIED JULY 26th 1929 AGED 78 YEARS R. I. P .ERECTED BY THEIR FOND SON THOMAS RUANE PHILADELPHIA USA}. If no memorial number or inscription is visible in an image,return a json with NULL in each field",
+              text: "You're an expert in OCR and are working in a heritage/genealogy context assisting in data processing post graveyard survey. Examine these images and extract the handwritten text from the inscription field for each memorial number - no other fields. Respond in JSON format only. e.g., {memorial_number: 69, inscription: SACRED HEART OF JESUS HAVE MERCY ON THE SOUL OF THOMAS RUANE LISNAGROOBE WHO DIED APRIL 16th 1923 AGED 74 YRS AND OF HIS WIFE MARGARET RUANE DIED JULY 26th 1929 AGED 78 YEARS R. I. P. ERECTED BY THEIR FOND SON THOMAS RUANE PHILADELPHIA USA}. If no memorial number or inscription is visible in an image, return a JSON with NULL in each field",
             },
             {
               type: "image_url",
@@ -97,52 +115,33 @@ async function processFile(filePath) {
 
 function storeResults(data) {
   const resultsPath = "./data/results.json";
-  const flagPath = "./data/processing_complete.flag";
-
   try {
-    // Log the raw data for debugging
-    console.log("Raw data from OpenAI:", JSON.stringify(data));
-
-    // Check if data.message and data.message.content exist
-    if (!data.message || !data.message.content) {
-      console.error("Invalid data format: missing message or content field");
-      return;
+    let existingResults = [];
+    if (fs.existsSync(resultsPath)) {
+      const data = fs.readFileSync(resultsPath, "utf8");
+      existingResults = JSON.parse(data);
     }
 
-    // Extract and clean the JSON string from the content field
     const rawJsonString = data.message.content
       .replace("```json\n", "")
       .replace("\n```", "")
       .trim();
 
-    // Log the extracted string for debugging
-    console.log("Extracted JSON string:", rawJsonString);
-
-    // Parse the JSON string
     let parsedData = JSON.parse(rawJsonString);
 
-    // Check if parsedData is an array; if not, make it an array
     if (!Array.isArray(parsedData)) {
-      parsedData = [parsedData]; // Convert to an array with a single element
+      parsedData = [parsedData];
     }
 
-    // Map over the parsed data to create a new array containing only the required fields
     const formattedData = parsedData.map((item) => ({
       memorial_number: item.memorial_number,
       inscription: item.inscription,
     }));
 
-    // Write the formatted data to results.json
-    fs.writeFileSync(
-      resultsPath,
-      JSON.stringify(formattedData, null, 2),
-      "utf8"
-    );
-    console.log("Stored formatted results in results.json.");
+    const allResults = existingResults.concat(formattedData);
 
-    // Set a flag to indicate processing is complete
-    fs.writeFileSync(flagPath, "complete");
-    console.log("Set processing completion flag.");
+    fs.writeFileSync(resultsPath, JSON.stringify(allResults, null, 2), "utf8");
+    console.log("Stored formatted results in results.json.");
   } catch (err) {
     console.error("Error in storeResults function:", err);
   }
@@ -150,16 +149,26 @@ function storeResults(data) {
 
 app.get("/processing-status", (req, res) => {
   const flagPath = "./data/processing_complete.flag";
+  const filesPath = "./uploads";
 
   try {
     if (fs.existsSync(flagPath)) {
       console.log("Processing complete, data available.");
-      res.json({ status: "complete" });
+      const processedFiles = fs.readdirSync(filesPath).length;
+      const progressPercentage = (processedFiles / totalFiles) * 100;
+
+      if (progressPercentage === 100) {
+        req.files.forEach((file) => {
+          fs.unlinkSync(file.path);
+        });
+      }
+
+      res.json({ status: "complete", progress: progressPercentage });
       fs.unlinkSync(flagPath);
       console.log("Processing completion flag cleared.");
     } else {
       console.log("Processing ongoing.");
-      res.json({ status: "processing" });
+      res.json({ status: "processing", progress: 0 });
     }
   } catch (err) {
     console.error("Error checking processing status:", err);
