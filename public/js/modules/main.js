f879ab0b7496e4eacd66a64553d720649398c205
dtcurragh <97108312+dtcurragh@users.noreply.github.com>
2024-07-11 11:11:28 +0100

    COmpelet modularisation of processing.html javascript by craiting main.js to handle caching DOM elements, attaching event listeners, and initializing the progress check.

---
+++ public/js/modules/main.js
@@ -0,0 +1,17 @@
+/* eslint-disable quotes */
+// main.js
+
+import { checkProgress } from "./api.js";
+import { handleCancelProcessing } from "./cancelProcessing.js";
+
+// Cache DOM element for cancel button
+const cancelProcessingButton = document.getElementById(
+  "cancelProcessingButton"
+);
+
+// Attach event listener to the cancel button
+cancelProcessingButton.addEventListener("click", handleCancelProcessing);
+
+// Initialize progress checking
+checkProgress();
+setInterval(checkProgress, 2000);
