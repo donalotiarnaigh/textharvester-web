719852759b5e8d36832a04c972ef149fed1a823e
dtcurragh <97108312+dtcurragh@users.noreply.github.com>
2024-07-15 18:53:49 +0100

    update authcontroller.js and authroutes.js for register and login apis

---
+++ src/routes/authRoutes.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const { check } = require('express-validator');
+const authController = require('../controllers/authController');
+
+router.post(
+  '/register',
+  [
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Password must be 6 or more characters').isLength({
+      min: 6,
+    }),
+  ],
+  authController.register
+);
+
+router.post(
+  '/login',
+  [
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Password is required').exists(),
+  ],
+  authController.login
+);
+
+module.exports = router;
